8863cfa06fbf802958d39fea7f63d88860fc5c83
unnathLS <boletoqlz@gmail.com>
2024-11-02 19:25:52 -0300

    Acho que agora consigo dar push na pasta correta

---
+++ nodeSchool/javascripting/if-statement.js
@@ -0,0 +1,15 @@
+const fruit = 'orange'
+if(fruit.length > 5){
+  console.log('The fruit name has more than five characters.')
+} else {
+  console.log('The fruit name has five characters or less.')
+}
+
+
+//Errado
+// let fruit = 'orange';
+// if(fruit > 5){
+//     console.log('The fruit name has five characters or less')
+// }else{
+//     console.log('The fruit name has more than five characters.' )
+// }
